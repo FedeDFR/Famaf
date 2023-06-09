fda25987c82c65bc326a236ed604df3c621c7f97
FedeDFR <126607262+FedeDFR@users.noreply.github.com>
2023-06-09 13:05:05 -0300

    Update

--- Segundo_año/Organizacion_del_computador/Proyecto/app.s
+++ Segundo_año/Organizacion_del_computador/Proyecto/app.s
@@ -36,6 +36,7 @@ main:
 	mov x14, xzr					// Cordenada de x
 	mov x15, xzr					// Cordenada de y
 	mov x10, xzr					// Color
+	mov x9, GPIO_BASE				// GPIO
 	mov x8, xzr						// Radio
 	movz x7, 0xFFFF, lsl 00 		// Tiempo
 	movk x7, 0xFFF, lsl 16
@@ -3981,6 +3982,7 @@ main:
 
 
 	Leer:
+
 		mov x9, GPIO_BASE				//Seteo x9 en la direccion base de gpio
 		
 		// Setea gpios 0 - 9 como lectura
@@ -3993,7 +3995,8 @@ main:
 
 		cmp w11, key_w
 		beq ejec_w
-	ret
+		
+	b Leer
 
 
 //------------------------------------------ Fin Input -----------------------------------------------------------//
@@ -4101,6 +4104,36 @@ main:
 
 
 //------------------------------------------ Fin Fondo Multiceleste ----------------------------------------------//
+
+
+//------------------------------------------ GPIOM ---------------------------------------------------------------//
+
+
+	// Ejemplo de uso de gpios
+	mov x9, GPIO_BASE
+
+	// Atención: se utilizan registros w porque la documentación de broadcom
+	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+
+	// Setea gpios 0 - 9 como lectura
+	str wzr, [x9, GPIO_GPFSEL0]
+
+	// Lee el estado de los GPIO 0 - 31
+	ldr w10, [x9, GPIO_GPLEV0]
+
+	// And bit a bit mantiene el resultado del bit 2 en w10 (notar 0b... es binario)
+	// al inmediato se lo refiere como "máscara" en este caso:
+	// - Al hacer AND revela el estado del bit 2
+	// - Al hacer OR "setea" el bit 2 en 1
+	// - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
+	and w11, w10, 0b00000010
+
+	// si w11 es 0 entonces el GPIO 1 estaba liberado
+	// de lo contrario será distinto de 0, (en este caso particular 2)
+	// significando que el GPIO 1 fue presionado
+
+
+//------------------------------------------ Fin GPIOM -----------------------------------------------------------//
 
 
 InfLoop:
