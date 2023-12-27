ce640e30e65ce267284817bff2d6b130ac973ef1
Federico Di Forte Reusa <fededifortereusa@mi.unc.edu.ar>
2023-12-27 20:06:04 -0300

    Problemas resueltos: Mayor_sub_array & Theatre_square

--- Programacion_competitiva/Way_too_long_words/way_too_long_words.cpp
+++ Programacion_competitiva/Way_too_long_words/way_too_long_words.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
+#define fore(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back
+#define sz(x) (int)x.size()
+#define fst first
+#define snd second 
+#define all(x) x.begin(),x.end()
+#define REGALO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
 using namespace std;
+typedef long long ll;
 
 int main() {
+    
+    REGALO;
+
     int n;
     cin >> n;
 
@@ -15,8 +27,7 @@ int main() {
         if(largo > 10) {
             cout << str[0] << largo-2 << str[largo-1] << endl;
         } else {
-            cout << str << endl;
+            cout << str << "\n";
         }
-
     }
 }
