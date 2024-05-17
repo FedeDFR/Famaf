77095fb75466a3a3d9afbf1f77dabe3542973148
Federico Di Forte Reusa <fededifortereusa@mi.unc.edu.ar>
2024-05-17 09:43:12 -0300

    lab03 de redes

---
+++ Tercer_año/Redes_y_sistemas_distribuidos/lab03/lab3-kickstarter/out/clang-release/Generator.o.d
@@ -0,0 +1,6 @@
+out/clang-release//Generator.o: Generator.cc \
+  /home/el-yankee/omnetpp-6.0.1/include/omnetpp.h NodeTx.cc
+
+/home/el-yankee/omnetpp-6.0.1/include/omnetpp.h:
+
+NodeTx.cc:
---
+++ Tercer_año/Redes_y_sistemas_distribuidos/lab03/lab3-kickstarter/out/clang-release/Sink.o.d
@@ -0,0 +1,6 @@
+out/clang-release//Sink.o: Sink.cc \
+  /home/el-yankee/omnetpp-6.0.1/include/omnetpp.h NodeRx.cc
+
+/home/el-yankee/omnetpp-6.0.1/include/omnetpp.h:
+
+NodeRx.cc:
